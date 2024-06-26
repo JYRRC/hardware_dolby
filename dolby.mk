cff3c6d11c48b03627029ff7edc931c6417df7c6
someone5678 <someone5678@users.noreply.github.com>
2024-06-25 23:58:55 -0300

    dolby: Import more dolby related props
    * Whether deprecated or not, it could be used on legacy platforms
      or even recent platform's prebuilt hals

--- dolby.mk
+++ dolby.mk
@@ -44,6 +44,8 @@ PRODUCT_COPY_FILES += \
 
 # Dolby
 PRODUCT_VENDOR_PROPERTIES += \
+    vendor.audio.dolby.ds2.enabled=true \
+    vendor.audio.dolby.ds2.hardbypass=false \
     ro.vendor.dolby.dax.version=DAX3_3.6.0.12_r1 \
     ro.vendor.product.device.db=OP_DEVICE \
     ro.vendor.product.manufacturer.db=OP_PHONE \
